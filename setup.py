1fac22e8943a423f5bca58b341c917bab9522978
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-07-19 23:03:41 -0400

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pygments-lexer-apex',
+      version='0.1.0',
+      description='Pygments Salesforce Apex lexer',
+      author='Shawn Wallis',
+      url='https://github.com/shawalli/pygments-lexer-apex',
+      py_modules=['pygments_lexer_apex']
+)
