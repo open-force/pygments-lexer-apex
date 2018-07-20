43860e60714d67ad83848c2502f1ce8d3a0076f9
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-07-19 23:21:39 -0400

    adds entry point (and other info) to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import setup
 
-setup(name='pygments-lexer-apex',
-      version='0.1.0',
-      description='Pygments Salesforce Apex lexer',
-      author='Shawn Wallis',
-      url='https://github.com/shawalli/pygments-lexer-apex',
-      py_modules=['pygments_lexer_apex']
+setup(
+    name='pygments-lexer-apex',
+    version='0.2.0',
+    url='https://github.com/shawalli/pygments-lexer-apex',
+    license='MIT',
+    author='Shawn Wallis',
+    author_email='shawn.p.wallis@gmail.com',
+    description='Pygments Salesforce Apex lexer',
+    keywords = 'syntax highlighting',
+    platforms = 'any',
+    py_modules=['pygments_lexer_apex'],
+    entry_points='[pygments.lexers]\napexlexer = pygments_lexer_apex:ApexLexer',
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Intended Audience :: Developers',
+      'Development Status :: 5 - Alpha',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 3',
+      'Operating System :: OS Independent',
+      'Topic :: Text Processing :: Filters',
+      'Topic :: Utilities'
+    ]
 )
